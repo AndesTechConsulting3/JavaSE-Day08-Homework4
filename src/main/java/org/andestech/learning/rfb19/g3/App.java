ab4a4cb8434d4d3d8801228e1a9a0753acf7752b
MihaRF <miharf@gmail.com>
2019-02-17 12:36:52 +0300

    ArrayList realisation

--- src/main/java/org/andestech/learning/rfb19/g3/App.java
+++ src/main/java/org/andestech/learning/rfb19/g3/App.java
@@ -1,13 +1,59 @@
 package org.andestech.learning.rfb19.g3;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import java.util.*;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ArrayList<Book> booklist = new ArrayList<>();
+        booklist.add(new Book("Николай Носов", "Незнайка учится", 1937));
+        booklist.add(new Book("Джанни Родари","Чиполлино" , 1961));
+        booklist.add(new Book("Джордж Оруэлл", "1.9.8.4.", 1949));
+        booklist.add(new Book("Рэй Брэдбери", "Марсианские хроники", 1950));
+
+        System.out.println(booklist);
+
+        booklist.sort(new Comparator<Book>() {
+            @Override
+            public int compare(Book o1, Book o2) {
+                return o1.title.compareTo(o2.title);
+            }
+        });
+        System.out.println(booklist);
+
+        booklist.sort(new Comparator<Book>() {
+            @Override
+            public int compare(Book o1, Book o2) {
+                return o1.author.compareTo(o2.author);
+            }
+        });
+        System.out.println(booklist);
+
+        booklist.sort(new Comparator<Book>() {
+            @Override
+            public int compare(Book o1, Book o2) {
+                return o1.year.compareTo(o2.year);
+            }
+        });
+        System.out.println(booklist);
+
+    }
+
+    private static class Book
+    {
+        String title, author;
+        Integer year;
+
+        public Book(String author, String title, int year) {
+            this.author = author;
+            this.title = title;
+            this.year = year;
+        }
+
+        public String toString(){
+            return title + ", " + author + ", " + year + ".\n";
+        }
+
     }
 }
